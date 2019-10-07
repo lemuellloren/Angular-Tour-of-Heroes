64dbeac728f9d1eff70d4cce85c1e39747ce68a4
Lemuel Lloren <lempakz@yahoo.com>
2019-10-07 14:03:17 +0800

    MessageService  replaced

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -4,6 +4,14 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class MessageService {
+  messages: string[] = [];
 
+  add(messages: string) {
+    this.messages.push(messages);
+  }
+
+  clear() {
+    this.messages = [];
+  }
   constructor() { }
 }
